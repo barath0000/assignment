a3d013f353798b21a076d104a7de27a18c082277
barath0000 <barathrnj@gmail.com>
2026-03-10 19:48:29 +0530

    Initial commit

---
+++ script3.py
@@ -0,0 +1,12 @@
+number=float(input("enter your number"))
+if number%2==0:
+    print("number is even")
+else:
+    print("number is odd")
+number=int(input("enter your number"))
+
+if number%2==0:
+    print("number is even")
+else:
+    print("number is odd")
+
